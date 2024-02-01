846e33f3b807ec4c3c2db64e6a2909a21771778c
Nirab Shrestha <074msccd011.nirab@pcampus.edu.np>
2024-01-31 23:01:16 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,17 @@ require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer"], (Map, Ma
     basemap: "dark-gray"
   });
 
-  const view = new MapView({
-    container: "viewDiv",
-    map: map,
-
-    extent: {
-      // autocasts as new Extent()
-      xmin: -9177811,
-      ymin: 4247000,
-      xmax: -9176791,
-      ymax: 4247784,
-      spatialReference: 102100
-    }
-  });
+const view = new MapView({
+  container: "viewDiv",
+  map: map,
+  extent: {
+    xmin: -10000000,
+    ymin: 2000000,
+    xmax: -7000000,
+    ymax: 8000000,
+    spatialReference: 102100
+  }
+});
 
   /********************
    * Add feature layer
