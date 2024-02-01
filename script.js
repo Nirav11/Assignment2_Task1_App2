6670be9a11c4f2a660dfe769c014912f0150a411
Nirab Shrestha <074msccd011.nirab@pcampus.edu.np>
2024-01-31 22:30:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer"], (Map, MapView, FeatureLayer) => {
   const map = new Map({
-    basemap: "hybrid"
+    basemap: "dark-gray"
   });
 
   const view = new MapView({
@@ -22,15 +22,56 @@ require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer"], (Map, Ma
    ********************/
 
   var featureLayer_1 = new FeatureLayer({
-url: "https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_Boundaries_2022/FeatureServer/1"
-});
+    url: "https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_Boundaries_2022/FeatureServer/1",
+    popupTemplate: {
+      title: "State: {STATE_NAME}",
+      content: [
+        {
+          type: "fields",
+          fieldInfos: [
+            {
+              fieldName: "STATE_ABBR",
+              label: "State Abbreviation",
+              visible: true
+            },
+            {
+              fieldName: "POPULATION_2022",
+              label: "Population 2022",
+              visible: true
+            },
+            // Add other fields as needed
+          ]
+        }
+      ]
+    }
+  });
 
+  map.add(featureLayer_1);
 
-map.add(featureLayer_1);
+  var featureLayer_2 = new FeatureLayer({
+    url: "https://services1.arcgis.com/fBc8EJBxQRMcHlei/arcgis/rest/services/NPS_Land_Resources_Division_Boundary_and_Tract_Data_Service/FeatureServer/2",
+    popupTemplate: {
+      title: "{Park Name}",
+      content: [
+        {
+          type: "fields",
+          fieldInfos: [
+            {
+              fieldName: "Unit Code",
+              label: "Unit Code",
+              visible: true
+            },
+            {
+              fieldName: "State",
+              label: "State",
+              visible: true
+            },
+            // Add other fields as needed
+          ]
+        }
+      ]
+    }
+  });
 
-var featureLayer_2 = new FeatureLayer({
-url: "https://services1.arcgis.com/fBc8EJBxQRMcHlei/arcgis/rest/services/NPS_Land_Resources_Division_Boundary_and_Tract_Data_Service/FeatureServer/2"
-});
-
-map.add(featureLayer_2);
+  map.add(featureLayer_2);
 });
